8ea079ec78a2ecc5915a0fc76361f05fcce62133
Martin Sandström <maartiin_91@hotmail.com>
2016-05-18 14:18:05 +0200

    first commit

---
+++ app/main.js
@@ -0,0 +1,36 @@
+$(function(){
+
+	var $calculate = $('#calculate');
+	var $counter = $('#counter');
+	var $attendees = $('#attendees');
+	var $cost = $('#cost');
+	var $stop = $('#stop');
+	var $result = $('#result');
+	var counter = 0;
+	var myInterval;
+
+	$result.hide();
+
+	$calculate.click(function(){
+		counter = 0;
+		$result.show();
+		var numberOfAttendees = $attendees.val();
+		var costPerAttendee = $cost.val();
+
+		var costPerSecond = (costPerAttendee / 3600) * numberOfAttendees;
+
+		myInterval = setInterval(function () {
+			++counter;
+			var newCost = counter * costPerSecond;
+			var newCostRound = Math.round(newCost * 100) / 100
+			$counter.text(newCostRound);
+		}, 1000);
+	});
+
+	$stop.click(function(){
+		clearInterval(myInterval);
+	});
+
+	// to stop the counter
+
+});
